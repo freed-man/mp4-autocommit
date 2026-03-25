af08404023b9f1593c95bb848867b90a975fe7a9
freed-man <rolandk@proton.me>
2026-03-25 13:00:00 +0000

    add sqlite3 fallback for local development

--- autocommit/settings.py
+++ autocommit/settings.py
@@ -85,9 +85,17 @@ WSGI_APPLICATION = 'autocommit.wsgi.application'
 
 # Database
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
