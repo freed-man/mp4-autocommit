942843d45ffb352ff29d9037af024f95ca664f80
freed-man <rolandk@proton.me>
2026-03-30 13:05:00 +0100

    add checkout order form and register order models in admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = (
+        'order_number', 'date', 'order_total',
+        'original_bag', 'stripe_pid',
+    )
+    fields = (
+        'order_number', 'user_profile', 'date',
+        'full_name', 'email', 'phone_number',
+        'vehicle_reg', 'order_total',
+        'original_bag', 'stripe_pid',
+    )
+    list_display = (
+        'order_number', 'date', 'full_name',
+        'order_total',
+    )
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
